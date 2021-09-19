71009111608584d4cf85c942df33f5c0de2d8ee5
otobakataro <vancyroletaka@gmail.com>
2021-09-19 22:59:56 +0900

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,33 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "goriblog",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-custom-api`,
+      options: {
+        apiKey: process.env.API_KEY,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`],
+        defaultLayouts: {
+          default: require.resolve(`./src/components/layout.js`)
+        }
+      },
+    },
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        preset: "@theme-ui/preset-funck",
+      },
+    }
+  ],
 };
